d4687b04eb0ceb642d1faa81504fd6feccec55ce
xandreithefrog <xandreithefrog@gmail.com>
2020-09-11 20:01:08 +0300

    tweaks

---
+++ eval.js
@@ -0,0 +1,24 @@
+const util = require('util');
+const info = require('./info.json');
+function clean(text) {
+    if (typeof(text) !== 'string') {
+        text = util.inspect(text, { depth: 0 });
+    }
+    text = text
+        .replace(/`/g, '`' + String.fromCharCode(8203))
+        .replace(/@/g, '@' + String.fromCharCode(8203))
+        .replace(info.login, 'die') 
+    return text;
+}
+
+module.exports = (message, code) => {
+    if(message.author.id !== info.owner) return;
+    try {
+        let evaled = eval(code);
+        if (typeof evaled !== "string")
+            evaled = util.inspect(evaled);
+            message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+}
